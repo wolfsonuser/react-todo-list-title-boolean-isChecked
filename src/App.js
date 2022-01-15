a42710818749e884bde4725fffb854d43387aff3
wolfsonuser At 52nv <nelson.lopez-jimenez@edmonds.edu>
2022-01-15 07:25:38 -0800

    proxy fixed, /api/todos get correct

--- src/App.js
+++ src/App.js
@@ -1,15 +1,58 @@
 //  Nelson Lopez
 // react-todo-list 01/15/2022
-import './App.css';
-const bootstrap = require('bootstrap');
+//import './App.css';
+import React from 'react';
+import {  Row, Col, InputGroup, Button, Form, Jumbotron } from 'bootstrap'
 
+// Set URL
+let url = '/api/todos'
 function App() {
-  const { Container, Row, Col } = bootstrap;
   return (
     <div className="App">
+      <TodoListCard />
       <h1>Hello World!!!</h1>
     </div>
   );
+}
+function TodoListCard() {
+  const [items, setItems] = React.useState(null);
+  console.log(`18 - `)
+  React.useEffect(() => {
+    fetch(url)
+      .then(response => response.json())
+      .then(setItems);
+  }, []);
+
+  if (items === null) return 'Loading.....';
+
+  return (
+    <React.Fragment>
+      {/* <AddItemForm onNewItem={onNewItem} /> */}
+      {/* <AddItemForm  /> */}
+      {items.length === 0 && (
+        <p className="text-center">You have no items yet!!</p>
+      )}
+      { items.map( item => (
+        <ItemDisplay
+           item = {item }
+           key = {item._id } // ERROR!!
+        />
+      ))}
+    </React.Fragment>
+  )
+}
+function AddItemForm ( { onNewItem }) {
+  const [newItem, setNewItem] = React.useState('');
+}
+
+function ItemDisplay ( { item }) {
+  console.log(`48 - ${item.title} ${item._id}`);
+  return (
+   <>
+      {item.title} {item._id } {'\n'}
+   </>
+    
+  )
 }
 
 export default App;
